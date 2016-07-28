3e8a48d31a16a5ac4e72e7bfb84487a0c145d0f9
Ignitelegal <bd@ignitelegal.com.au>
2016-07-28 19:51:29 +1000

    Create Book 1 with JS
    Populates div1 with object(book1) values, including link to img file
    (with class id styled to fit in div)

--- WebstoreHack/js/stocklist.js
+++ WebstoreHack/js/stocklist.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 var content1 = {
 	id : "book_1", 
 	title : "Code Red",
@@ -10,7 +6,7 @@ var content1 = {
 	price : 15.95,
 	highlights : ["Action-hacked", " cybercrime-fighting", " ginger ninga"],
 	picture_url : "http://www.loopdesigngroup.com/wordpress/wp-content/uploads/Print_CodeRed.jpg" //style="height:80px"//
-};
+}
 
 var content2 = {
 	id : "book_2",
@@ -19,7 +15,7 @@ var content2 = {
 	price : 25.95,
 	highlights : ["Thought code was whack?", " Methodone for coders"],
 	picture_url : "http://thetinylife.com/wp-content/uploads/2013/01/book-cover.jpg" //style="height:80px"
-};
+}
 
 var content3 = {
 	id : "book_3",
@@ -29,7 +25,7 @@ var content3 = {
 	highlights : ["Easy", " step-by-step guide", " with lots of useless drawings"],
 	picture_url : "http://www.sean.co.uk/shared/gfx/bookcovers/superskills250x333.jpg" //style="height:80px"
 
-};
+}
 
 var content4 = {
 	id : "album_1",
@@ -38,7 +34,7 @@ var content4 = {
 	price : 4.95,
 	highlights : ["If you think Trump is a dick...", " meet his Hair-god!", " - Douchebag Heaven"],
 	picture_url : "https://s-media-cache-ak0.pinimg.com/236x/e8/8a/bb/e88abbb79808fc2b50a25ae48cc41542.jpg" //style="height:80px">
-};
+}
 
 var content5 = {
 	id : "album_2",
@@ -47,38 +43,43 @@ var content5 = {
 	price : 5.95,
 	highlights : ["Accordian-plaing", "Rangas", "- Hot Stuff!"],
 	picture_url : "https://s-media-cache-ak0.pinimg.com/564x/63/e6/65/63e665a454bd6c4fa2ab86a114966ac8.jpg" //style="height:80px">
-};
+}
 
+var books = [
+	Object(content1), Object(content2), Object(content3)
+]
 
-
-var book_1 = Object(content1);
-
-var book_2 = Object(content2);
-
-var book_3= Object(content3);
-
-var album_1= Object(content4);
-
-var album_2= Object(content5);
+var albums = [
+	Object(content4), Object(content5)
+]
 
 //^ DO I ACTUALLY NEED THIS? ^ //
 
-
+var setSize = function (pic){
+	pic.html('<style="height:80px">');
+}
 
 var t = $('<li>');
-t.html('Title :'+ " " + book_1.name);
+t.html('Title :'+ " " + content1['title']);
+
 var c = $('<li>');
-c.html('Category :'+ " " + book_1.category);
+c.html('Category :'+ " " + content1['category']);
+
 var p = $('<li>');
-p.html('Price :'+ " " + "$" + book_1.price);
+p.html('Price :'+ " " + "$" + content1['price']);
+
 var h = $('<li>');
-h.html('Highlights :'+ " " + book_1.highlights); 
+h.html('Highlights :'+ " " + content1['highlights']); 
+
+var l = $('<div>')
+l.html("<img class='pic' src =" + content1['picture_url'] + ">"); //id=pic
 
 var addContent = function() {
 	$("#book1").append(t);
 	$("#book1").append(c);
 	$("#book1").append(p);
 	$("#book1").append(h);
+	$("#book1").append(l);
 }
 
 	
@@ -95,18 +96,18 @@ $(function(){
 $('#nameBook1').html(.append('testContent');
 */
 
-/*
-var content = $(book_1);
+
+/* var content = $(books);
 
 var addToPage = function(content) {
 	append("<div id=" + object["id"] + " class=" + object["category"] + " </div>");
-			$("#" + object["id"]).append("<div class='name'>"+ object["name"] +"</div>");
+			$("#" + object["id"]).append("<div class='title'>"+ object["title"] +"</div>");
 			$("#" + object["id"]).append("<div class='category'>"+ object["category"] +"</div>");
 			$("#" + object["id"]).append("<div class='price'>"+ object["price"] +"</div>");
 			$("#" + object["id"]).append("<div class='highlights'>"+ object["highlights"] +"</div>");
 			$("#" + object["id"]).append("<div class='picture_url'>"+ object["picture_url"] + "</div>");
 };
 
-$("#book1").addToPage;
-*/
+$("#book1").addToPage; */
+
 
