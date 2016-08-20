ec15d6a11c055e6de26821b55126a3a2f4809889
Ignitelegal <bd@ignitelegal.com.au>
2016-08-21 03:06:21 +1000

    AddContent with JS

--- WebstoreHack/js/stocklist.js
+++ WebstoreHack/js/stocklist.js
@@ -1,6 +1,7 @@
 
 var content1 = {
-	id : "book1", 
+	id : 'content1',
+	type : 'book', 
 	title : "Code Red",
 	category : "Action",
 	price : 15.95,
@@ -9,16 +10,18 @@ var content1 = {
 }
 
 var content2 = {
-	id : "book2",
+	id : 'content2',
+	type : 'book',
 	title : "Cracking the Code",
 	category : "Mystery",
 	price : 25.95,
-	highlights : ["Thought code was whack?", " Methodone for coders"],
+	highlights : ["Thought code was whack?", " Youll need rehab after this one!"],
 	picture_url : "http://thetinylife.com/wp-content/uploads/2013/01/book-cover.jpg" //style="height:80px"
 }
 
 var content3 = {
-	id : "book3",
+	id : 'content3',
+	type : 'book',
 	title : "How to Code",
 	category : "DIY",
 	price : 35.95,
@@ -28,95 +31,181 @@ var content3 = {
 }
 
 var content4 = {
-	id : "album1",
+	id : 'content4',
+	type : 'album',
 	title : "Going back to Miami",
 	category : "Country",
 	price : 4.95,
-	highlights : ["If you think Trump is a dick...", " meet his Hair-god!", " - Douchebag Heaven"],
+	highlights : ["Think Trump is a dick?", " ...meet his Hair-god!", " - Douchebag Heaven"],
 	picture_url : "https://s-media-cache-ak0.pinimg.com/236x/e8/8a/bb/e88abbb79808fc2b50a25ae48cc41542.jpg" //style="height:80px">
 }
 
 var content5 = {
-	id : "album2",
+	id : 'content5',
+	type : 'album',
 	title : "The Currie Brothers",
 	category : "Tragic",
 	price : 5.95,
-	highlights : ["Accordian-plaing", "Rangas", "- Hot Stuff!"],
+	highlights : ["Accordian-playing", " Ranga twins", " Dont take our word for it, these boys are - Hot Stuff!"],
 	picture_url : "https://s-media-cache-ak0.pinimg.com/564x/63/e6/65/63e665a454bd6c4fa2ab86a114966ac8.jpg" //style="height:80px">
 }
 
-var books = [
-	Object(content1), Object(content2), Object(content3)
-]
-
-var albums = [
-	Object(content4), Object(content5)
-]
-
+//create array containing all products - use for stock availability//
 var products = [
-	Object(books), Object(albums)
-]
+	Object(content1), 
+	Object(content2), 
+	Object(content3),
+	Object(content4),
+	Object(content5)
+];
+
+//sort current products by type (and push to book or album array)//
+var books = [];
+var albums = [];
+function sortProduct(){
+	for (var i=0; i<products.length; i++){
+		var eachProduct = products[i];
+			if (eachProduct.type === 'book') {
+			books.push(eachProduct);
+			} 
+			else {
+			albums.push(eachProduct);
+			};
+		};
+};
 
 
-var addContent = function(){
-	for (i=0; i<products.length; i++){
-		console.log(products[i]);
+$(window).load(function(){
+	$('#bookshelf').css({'display':'none'});
+	$('#musicshelf').css({'display':'none'});
+	sortProduct();
+	showBooks();
+	showAlbums();//
+	});	
+
+
+var showAlbums = function(){
+	$('#musicshelf').css({'display':'block'});
+	$('#bookshelf').css({'display':'hidden'});
+	for (var i=0; i<albums.length; i++){
+		var product = albums[i];
+  		var $newProduct = $('<div>').addClass('col-4').attr('id', product.id);
+  		$('div #musicshelf').append($newProduct);
+
+ 		var l = $('<div>');
+			l.html("<img class='pic' src =" + product.picture_url + ">");
+				$("#" + product.id).append(l);
+
+ 		var t = $('<li>');
+			t.html('Title :'+ " " + product.title);
+				$("#" + product.id).append(t);
+
+		var c = $('<li>');
+			c.html('Category :'+ " " + product.category);
+				$("#" + product.id).append(c);
+
+		var p = $('<li>');
+			p.html('Price :  '+ " " + "$" + product.price);
+				$("#" + product.id).append(p);
+
+		var h = $('<li>');
+			h.html('Highlights :'+ " " + product.highlights); 
+				$("#" + product.id).append(h);
+
+
+		};
+	};
+
+
+var showBooks = function(){
+	$('#bookshelf').css({'display':'block'});
+	$('#musicshelf').css({'display':'hidden'});
+	for (var i=0; i<books.length; i++){
+		var product = books[i];
+  		var $newProduct = $('<div>').addClass('col-4').attr('id', product.id);
+  		$('div #bookshelf').append($newProduct);
 		
-		$("<div id="+ products[i] + "</div>").append();			
-			var t = $('<li>');
-			t.html('Title :'+ " " + products[i]['title']);
-			$('"#" + products[i]["id"]').append(t);
+  		var l = $('<div>');
+			l.html("<img class='pic' src =" + product.picture_url + ">");
+				$("#" + product.id).append(l);
 
-			var c = $('<li>');
-			c.html('Category :'+ " " + products[i]['category']);
-			$('"#" + products[i]["id"]').append(c);
+		var t = $('<li>');
+			t.html('Title :'+ " " + product.title);
+				$("#" + product.id).append(t);
 
-			var p = $('<li>');
-			p.html('Price :  '+ " " + "$" + products[i]['price']);
-			$('"#" + products[i]["id"]').append(p);
+		var c = $('<li>');
+			c.html('Category :'+ " " + product.category);
+				$("#" + product.id).append(c);
 
-			var h = $('<li>');
-			h.html('Highlights :'+ " " + products[i]['highlights']); 
-			$('"#" + products[i]["id"]').append(h);
-
-			var l = $('<div>')
-			l.html("<img class='pic' src =" + products[i]['picture_url'] + ">");
-			$('"#" + products[i]["id"]').append(l);
+		var p = $('<li>');
+			p.html('Price :  '+ " " + "$" + product.price);
+				$("#" + product.id).append(p);
+	
+		var h = $('<li>');
+			h.html('Highlights :'+ " " + product.highlights); 
+				$("#" + product.id).append(h);
 	
 		};
 	};
 
-window.onload = addContent()
 
 
- 
+/*
 
-	
-			//HOW DO I INSERT THE ACUTAL KEY VALUES FOR EACH OBJECT???//
+//COULD PROBABLY USE A SINGLE ADDCONTENT FUNCTION FOR EITHER PRODUCT TYPE
+
+	  var addContent = function(obj){
+			$("<div id=" + obj.id + "</div>").append();			
+
+				var t = $('<li>');
+				t.html('Title :'+ " " + product['title']);
+				$('"#" + obj.id').append(t);
+
+				var c = $('<li>');
+				c.html('Category :'+ " " + product['category']);
+				$('"#" + obj.id').append(c);
+
+				var p = $('<li>');
+				p.html('Price :  '+ " " + "$" + product['price']);
+				$('"#" + obj.id').append(p);
+
+				var h = $('<li>');
+				h.html('Highlights :'+ " " + product['highlights']); 
+				$('"#" + obj.id').append(h);
+
+				var l = $('<div>');
+				l.html("<img class='pic' src =" + product['picture_url'] + ">");
+				$('"#" + obj.id').append(l);
+			};
+		$(product).addContent();
+*/		
 
 
+//CANT MAKE THE NAV CLICK TO EXECUTE FUNCTION//
+
+$('#booklist').click(function(){ 
+	console.log("working");
+	//$('.nav li').attr('class','');
+  	//$('.nav li #booklist').addClass('active');
+  //	$('#musicshelf').css({'display':'none'});
+  //	$('#bookshelf').css({'display':'block'});
+ });
 
 
+$('#albumlist').click(function(){
+  $('.nav li').attr('class','');
+  $('.nav li #albumlist').addClass('active');
+  $('div #musicshelf').show();
+  $('div #bookshelf').hide();
+})
 
-/* 
-$(function(){
-	$("#book1").html('<img src="http://www.loopdesigngroup.com/wordpress/wp-content/uploads/Print_CodeRed.jpg" style="height:80px" />')
-}
-$('#nameBook1').html(.append('testContent');
+
+/*
+$('.nav #all').click(function(){
+  $('.nav li').attr('class','');
+  $('#all').addClass('active');
+  $('.product').show();
+})
 */
-
-
-/* var content = $(books);
-
-var addToPage = function(content) {
-	append("<div id=" + object["id"] + " class=" + object["category"] + " </div>");
-			$("#" + object["id"]).append("<div class='title'>"+ object["title"] +"</div>");
-			$("#" + object["id"]).append("<div class='category'>"+ object["category"] +"</div>");
-			$("#" + object["id"]).append("<div class='price'>"+ object["price"] +"</div>");
-			$("#" + object["id"]).append("<div class='highlights'>"+ object["highlights"] +"</div>");
-			$("#" + object["id"]).append("<div class='picture_url'>"+ object["picture_url"] + "</div>");
-};
-
-$("#book1").addToPage; */
 
 
